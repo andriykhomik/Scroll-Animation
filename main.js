eeb781c18b2465a51732b55d4ee7d067555986cc
andriykhomik <andriykhomik1992@gmail.com>
2021-11-06 21:42:44 +0200

    created togle for change bacground of boxes

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
-
-// checkBoxes();
-console.log (window.innerHeight);
+boxes.forEach((el)=>{
+    let isClicked = true;
+    const h2 = el.getElementsByTagName('h2');
+    el.addEventListener('click', ()=>{
+        if (isClicked){
+            background(el, h2);
+        } else {
+            el.style.background = '';
+            h2[0].style.display = 'flex'
+        }
+        isClicked = !isClicked;
+    })
+})
 
 function checkBoxes(){
     const triggerBottom = window.innerHeight / 5 * 3.7;
 
     boxes.forEach((box)=>{
         const boxTop = box.getBoundingClientRect().top;
+
         if (boxTop < triggerBottom){
-            console.log (boxTop);
-            console.log (triggerBottom);
             box.classList.add('show');
         } else {
             box.classList.remove('show');
         }
-
     })
+}
+
+function background(el, h2){
+    h2[0].style.display = 'none';
+    el.style.background = 'URL(image/20190616_142632.jpg)';
 }
